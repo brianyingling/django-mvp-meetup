821327f2cae757b0b39655d08779228c94732cc3
Brian Yingling <brian_yingling@yahoo.com>
2013-05-23 19:23:09 -0400

    init commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/brianyingling/Projects/django/django-mvp-meetup/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
